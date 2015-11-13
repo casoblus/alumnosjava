e0f9bf6a6a71794c9ccc0f4c9fdb04a558ab55ec
casoblus <casobrinolusquinos@gmail.com>
2015-11-13 23:13:05 +0100

    Los datos del alumno son recogidos por la clase Alumno

--- Alumno.java
+++ Alumno.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
 class Alumno
 {
 	private String nome;
 	private String dni;
 	private float nota;
 
-	public void ALTA( String nom, String dn, float not ) 
-	{
-		nome = nom;
-		dni = dn;
-		nota = not;
+	public void ALTA() 
+	{	
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\t****************************************");
+		System.out.println("\t*            ALTA DE ALUMNOS           *");
+		System.out.println("\t****************************************");
+		System.out.println("");
+		System.out.println("\t>> Nombre del alumno: ");
+		nome = sc.nextLine();
+		System.out.println("\t>> DNI del alumno: ");
+		dni = sc.nextLine();
+		System.out.println("\t>> Nota del alumno: ");
+		nota = sc.nextFloat();
+		System.out.println("\t");
 	}
 
 	public void MOSTRAR()
